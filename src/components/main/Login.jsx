f59c54248e5d581e0f1c70081e8bf13e9fda36d1
sohag <sohagmondal987@gmail.com>
2026-03-24 01:09:22 +0600

    jsx error found

--- src/components/main/Login.jsx
+++ src/components/main/Login.jsx
@@ -9,7 +9,7 @@ const Toast = ({ message }) => (
 );
 
 const Login = () => {
-  const API = import.meta.vite.env.VITE_API_URL;
+  const API = import.meta.env.VITE_API_URL;
   const navigate = useNavigate();
   const [formData, setFormData] = useState({ email: "", password: "" });
   const [error, setError] = useState(null);
